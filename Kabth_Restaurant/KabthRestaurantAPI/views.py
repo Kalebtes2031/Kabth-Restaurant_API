3be0809b1d618a459f9b9372a932165efee01da9
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2024-07-25 15:27:24 +0300

    added all the functionality of add to cart, and order

--- Kabth_Restaurant/KabthRestaurantAPI/views.py
+++ Kabth_Restaurant/KabthRestaurantAPI/views.py
@@ -17,30 +17,17 @@ from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 import requests, logging
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed
+
 from django.contrib import messages
 from django.contrib.auth import logout as auth_logout
 from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+
 
 
 logger = logging.getLogger(__name__)
 
-
-def custom_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.error(request, 'Invalid credentials')
-    return render(request, 'login.html')
-
-@login_required(login_url='/login/') 
-def home(request):
-    return render(request, 'home.html')
 
 @api_view(['POST'])
 def custom_logout(request):
@@ -49,8 +36,6 @@ def custom_logout(request):
     request.session.flush()
     logger.info("User logged out")
     return JsonResponse({'message': 'Logged out successfully'}, status=204)
-
-
 
 
 class LoginView(View):
@@ -71,30 +56,15 @@ class LoginView(View):
                 form.add_error(None, 'Incorrect username or password')
         return render(request, 'login.html', {'form': form})
     
-class CustomLogoutView(View):
-    def post(self, request):
-        if hasattr(request.user, 'auth_token'):
-            headers = {
-                'Authorization': f'Token {request.user.auth_token.key}'  # Use user's token
-            }
-            response = requests.post(f'{settings.BASE_URL}/auth/token/logout/', headers=headers)
-
-            if response.status_code == 204:
-                logout(request)  # Log out the user
-                return redirect('login')  # Redirect to login page
-            else:
-                # Debugging response
-                return HttpResponse(f"Logout failed with status {response.status_code}", status=response.status_code)
-        else:
-            logout(request)
-            return redirect('login')
 
 class HomeView(LoginRequiredMixin, View):
     def get(self, request):
         return render(request, 'home.html')
 
+@login_required
 def about(request):
     return render(request, 'about.html')
+
 
 @method_decorator(login_required, name='dispatch')
 class MenuItemView(generics.ListCreateAPIView):
@@ -130,26 +100,6 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
         menu_item = get_object_or_404(MenuItem, pk=pk)
         return render(request, "menu_item.html", {"menu_item": menu_item})
 
-# class MenuItemView(generics.ListAPIView, generics.ListCreateAPIView):
-#     queryset = MenuItem.objects.all()
-#     serializer_class = MenuItemSerializer
-#     ordering_fields = ['price']
-#     search_fields = ['title']
-#     throttle_classes = [AnonRateThrottle, UserRateThrottle]
-#     def get_permissions(self):
-#         if self.request.method == 'POST':
-#             return [IsAdminUser()]
-#         return [AllowAny()]
-    
-# class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView, generics.RetrieveAPIView):
-#     queryset = MenuItem.objects.all()
-#     serializer_class = MenuItemSerializer
-#     throttle_classes = [AnonRateThrottle, UserRateThrottle]
-#     def get_permissions(self):
-#         if self.request.method == 'POST' or self.request.method == 'PUT' \
-#                 or self.request.method == 'DELETE' or self.request.method == 'PATCH':
-#             return [IsAdminUser()]
-#         return [AllowAny()]
 
 @method_decorator(login_required, name='dispatch')   
 class Carts(generics.ListCreateAPIView):
@@ -159,33 +109,77 @@ class Carts(generics.ListCreateAPIView):
         user = self.request.user
         return Cart.objects.filter(user=user)
 
-    def perform_create(self, serializer):
-        menuitem = self.request.data.get('menuitem')
-        quantity = self.request.data.get('quantity')
-        unit_price = MenuItem.objects.get(pk=menuitem).price
-        quantity = int(quantity)
+    # def perform_create(self, serializer):
+    #     menuitem = self.request.data.get('menuitem')
+    #     quantity = self.request.data.get('quantity')
+    #     unit_price = MenuItem.objects.get(pk=menuitem).price
+    #     quantity = int(quantity)
+    #     price = quantity * unit_price
+    #     serializer.save(user=self.request.user, price=price)
+
+    def post(self, request, *args, **kwargs):
+        menuitem_id = request.POST.get('menuitem')
+        quantity = request.POST.get('quantity')
+
+        if not menuitem_id or not quantity:
+            return Response({"error": "Menu item and quantity are required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            menuitem = MenuItem.objects.get(pk=menuitem_id)
+            quantity = int(quantity)
+        except (MenuItem.DoesNotExist, ValueError):
+            return Response({"error": "Invalid menu item or quantity."}, status=status.HTTP_400_BAD_REQUEST)
+
+        unit_price = menuitem.price
         price = quantity * unit_price
-        serializer.save(user=self.request.user, price=price)
+
+        # Check if the item already exists in the cart
+        cart_item, created = Cart.objects.get_or_create(
+            user=request.user,
+            menuitem_id=menuitem_id,
+            defaults={'quantity': quantity, 'price': price}
+        )
+
+        if not created:
+            # If the cart item already exists, update the quantity and price
+            cart_item.quantity += quantity
+            cart_item.price += price
+            cart_item.save()
+
+        return redirect('cart')
+
 
     def delete(self, request):
         user = self.request.user
         Cart.objects.filter(user=user).delete()
         return Response(status=204)
     def list(self, request, *args, **kwargs):
-        cart_items = Cart.objects.filter(user=self.request.user)
+        cart_items = Cart.objects.filter(user=self.request.user).select_related('menuitem')
         return render(request, 'cart.html', {"cart_items": cart_items})
     
+
+@login_required
+def delete_cart_item(request, item_id):
+    cart_item = get_object_or_404(Cart, id=item_id, user=request.user)
+    cart_item.delete()
+    return redirect('cart')
+
 @method_decorator(login_required, name='dispatch')
 class OrdersView(generics.ListCreateAPIView):
     serializer_class = UserOrdersSerializer
     permission_classes = [IsAuthenticated]
     throttle_classes = [AnonRateThrottle, UserRateThrottle]
     
-    def perform_create(self, serializer):
+    def create(self, request, *args, **kwargs):
         cart_items = Cart.objects.filter(user=self.request.user)
-        total = self.calculate_total(cart_items)
-        order = serializer.save(user=self.request.user, total=total)
+        if not cart_items.exists():
+            return Response({"error": "Cart is empty"}, status=status.HTTP_400_BAD_REQUEST)
 
+        total = self.calculate_total(cart_items)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        order = serializer.save(user=self.request.user, total=total)
+        
         for cart_item in cart_items:
             OrderItem.objects.create(
                 menuitem=cart_item.menuitem, 
@@ -194,7 +188,8 @@ class OrdersView(generics.ListCreateAPIView):
                 price=cart_item.price, order=order
                 )
             cart_item.delete()
-
+        return redirect('orders')
+    
     def get_queryset(self):
         user = self.request.user
         if user.groups.filter(name='Manager').exists():
@@ -207,6 +202,21 @@ class OrdersView(generics.ListCreateAPIView):
             total += item.price
         return total
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        context = {
+            'orders': queryset
+        }
+        return render(request, 'orders.html', context)
+    
+@login_required
+def delete_order(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    if order.user == request.user or request.user.groups.filter(name='Manager').exists():
+        order.delete()
+    return redirect('orders')
+
+
 class SingleOrderView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = UserOrdersSerializer
     permission_classes = [IsAuthenticated]
@@ -215,6 +225,18 @@ class SingleOrderView(generics.RetrieveUpdateDestroyAPIView):
         if user.groups.filter(name='Manager').exists():
             return Order.objects.all()
         return Order.objects.filter(user=user)
+
+    def get(self, request, *args, **kwargs):
+        # Retrieve the order object
+        order = self.get_object()
+        order_items = OrderItem.objects.filter(order=order)
+        
+        # Render the template for a regular HTTP request
+        context = {
+            'order': order,
+            'order_items': order_items,
+        }
+        return render(request, 'order_item.html', context)
 
 class ManagerUsersView(generics.ListCreateAPIView):
     serializer_class = UserSerializer
@@ -263,3 +285,24 @@ class SingleDeliveryCrewView(generics.RetrieveDestroyAPIView):
         delivery_group = Group.objects.get(name='Delivery crew')
         queryset = User.objects.filter(groups=delivery_group)
         return queryset
+
+# class MenuItemView(generics.ListAPIView, generics.ListCreateAPIView):
+#     queryset = MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
+#     ordering_fields = ['price']
+#     search_fields = ['title']
+#     throttle_classes = [AnonRateThrottle, UserRateThrottle]
+#     def get_permissions(self):
+#         if self.request.method == 'POST':
+#             return [IsAdminUser()]
+#         return [AllowAny()]
+    
+# class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView, generics.RetrieveAPIView):
+#     queryset = MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
+#     throttle_classes = [AnonRateThrottle, UserRateThrottle]
+#     def get_permissions(self):
+#         if self.request.method == 'POST' or self.request.method == 'PUT' \
+#                 or self.request.method == 'DELETE' or self.request.method == 'PATCH':
+#             return [IsAdminUser()]
+#         return [AllowAny()]
